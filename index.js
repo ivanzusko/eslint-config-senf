c9106f149f2125d20a6bda9cc9082859856c0123
Pepijn Senders <ps@hellofresh.com>
2016-08-10 16:49:12 +0200

    Resolve airbnb dependency

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 module.exports = {
-    extends: 'airbnb',
-    env: {
-        browser: true,
-        node: true,
-        mocha: true,
-    },
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module',
-        ecmaFeatures: {
-            forOf: true,
-            jsx: true,
-            es6: true,
-            experimentalObjectRestSpread: true,
-        },
-    },
-    rules: {
-        indent: [
-            2,
-            4,
-            {
-                SwitchCase: 1,
-            },
-        ],
-        'react/jsx-indent': 0,
-        'react/react-in-jsx-scope': 0,
-        'react/jsx-indent-props': 0,
-        'no-use-before-define': 0,
-    },
-    plugins: [
-        'react',
-    ],
-    globals: {},
-    settings: {
-        'import/resolver': {
-            'babel-module-alias': {},
-        },
-    },
+    extends: [
+        'eslint-config-airbnb',
+    ].map(require.resolve),
+    // env: {
+    //     browser: true,
+    //     node: true,
+    //     mocha: true,
+    // },
+    // parserOptions: {
+    //     ecmaVersion: 6,
+    //     sourceType: 'module',
+    //     ecmaFeatures: {
+    //         forOf: true,
+    //         jsx: true,
+    //         es6: true,
+    //         experimentalObjectRestSpread: true,
+    //     },
+    // },
+    // rules: {
+    //     indent: [
+    //         2,
+    //         4,
+    //         {
+    //             SwitchCase: 1,
+    //         },
+    //     ],
+    //     'react/jsx-indent': 0,
+    //     'react/react-in-jsx-scope': 0,
+    //     'react/jsx-indent-props': 0,
+    //     'no-use-before-define': 0,
+    // },
+    // plugins: [
+    //     'react',
+    // ],
+    // globals: {},
+    // settings: {
+    //     'import/resolver': {
+    //         'babel-module-alias': {},
+    //     },
+    // },
 };
